403f912763e3931400e86b79982a7f46ac5967c1
Urvi Desai <urvidesai028@gmail.com>
2017-04-12 17:47:45 -0400

    Create dstar1.py

---
+++ dstar1.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import rospy
+from geometry_msgs.msg import Twist
+from nav_msgs.msg import Odometry
+import tf
+import math
+import numpy as np
+from std_msgs.msg import String
+from std_msgs.msg import Int32
+
+class dstar()
+  def __init__(self)
+
+    
+
+  def initialize()
+
+    
+# initialize open, close as lists
+# predecessors, successors, g, rhs as two-dimensional arrays and cost as one-dimensional array.
+# k_m, k_old ??
+# heuritsic will be a function
