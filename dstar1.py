e773b13e687fbbb1181a41bd0ae4f758cfe1f5bb
Urvi Desai <urvidesai028@gmail.com>
2017-04-14 16:47:44 -0400

    Update dstar1.py

--- dstar1.py
+++ dstar1.py
@@ -13,18 +13,20 @@ class dstar1():
   def __init__(self):
     
     # Name this node, it must be unique
-    rospy.init_node('dstar', anonymous=True)
+    rospy.init_node('dstar1', anonymous=True)
         
     # Enable shutdown in rospy
 #    rospy.on_shutdown(self.shutdown)
 
     # Read input data
+
+    # Read nodes, start, goal position and edge costs from input.txt file
     self.source = []
     self.dest = []
     self.cost = []
-    cnt = len(open('/home/urvi/mini_project/src/ROS_MINI_PROJECT/src/ros_demo/src/input.txt').readlines(  ))
+    cnt = len(open('/home/urvi/mini_project/ROS_MINI_PROJECT/src/ros_demo/src/input.txt').readlines(  ))
 
-    f1 = open('/home/urvi/mini_project/src/ROS_MINI_PROJECT/src/ros_demo/src/input.txt','r')
+    f1 = open('/home/urvi/mini_project/ROS_MINI_PROJECT/src/ros_demo/src/input.txt','r')
     self.n = int(f1.readline())
     self.Xo = int(f1.readline())
     self.Xg = int(f1.readline())
@@ -34,22 +36,13 @@ class dstar1():
     self.cost = np.empty([cnt-3,1])
     self.source,self.dest,self.cost = np.loadtxt(f1, unpack=True)
 
-#    for l in f1:
-#      row=l.split()
-#      self.source.append(row[0])
-#      self.dest.append(row[1])
-#      self.cost.append(row[2])
     f1.close()
 
+    # Read coordinates from coord.txt file
     self.x_co = np.empty([self.n,1])
     self.y_co = np.empty([self.n,1])
 
-    f1 = open('/home/urvi/mini_project/src/ROS_MINI_PROJECT/src/ros_demo/src/coords.txt')
-#    for l in f1:
-#      row=l.split()
-#      self.x_co.append(row[0])
-#      self.y_co.append(row[1])
-#    f1.close()
+    f1 = open('/home/urvi/mini_project/ROS_MINI_PROJECT/src/ros_demo/src/coords.txt')
 
     self.x_co,self.y_co = np.loadtxt(f1, unpack=True)
 
@@ -59,35 +52,30 @@ class dstar1():
     self.open = []
     self.close = []
     self.k_m = 0
+    self.g  = np.empty([self.n,1])
+    self.rhs = np.empty([self.n,1])
 
     # Establish publishers and subscribers
     
     self.currentnode_sub = rospy.Subscriber("/current_node",String,self.callback)
-#    self.goal_pub = rospy.Publisher("/goal",Int32,queue_size = 1)    
+    self.goal_pub = rospy.Publisher("/goal",Int32,queue_size = 1)    
 
 
-#  def main1(self):
-#    self.n1 = getNeighbor(self.vertices[0],self)
-#    return self.n1
-#    self.H = h(self.Xo,self.Xg,self)
-#    return self.H, self.n1
 
-#  def getG(u,self):
-#    if (u==Xg):
-#      return 0
-#    else:
-#      m =
       
-  
+
+
   def callback(self,data):
     
 #    for x in (1,self.vertices.size-1):
-      # returns neighbors as the index in dest array 
+
+     # returns neighbors as the index in dest array 
     def getNeighbor(u,self):
 
       self.ind = [k for (k,val) in enumerate(self.source) if val == u]
       return self.ind
 
+    # returns eucildean distance as heuristic
     def h(x1,x2,self):
 
       indx1 = self.x_co[int(x1)-1]
@@ -97,6 +85,15 @@ class dstar1():
 
       return math.sqrt((self.x_co[indx1]-self.x_co[indx2])**2 + (self.y_co[indy1]-self.y_co[indy2])**2)
 
+    def getG(u,self):
+      if (u == Xg):
+        return 0
+      else:
+        index = getNeighbor(u,self)
+        for l in len(index):
+          g_new.append = self.cost[index(l)] + self.g[inedx(l)]
+      
+        print g_new  
 
     def shutdown(self):
       rospy.sleep(0.1)
@@ -106,9 +103,7 @@ class dstar1():
 
 #       rospy.sleep(1)
     
-#    while not rospy.is_shutdown():
-#      n1 = getNeighbor(self.vertices[1],self)
-#      H = h(self.Xo,self.Xg,self)
+
      
 #  def update_vertex(u):
 
